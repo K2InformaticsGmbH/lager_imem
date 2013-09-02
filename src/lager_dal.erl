f9b89afb9bf3d36c4244db120ed28c04aa5d9b8e
Stefan Ochsenbein <ochsenbein@bluewin.ch>
2013-09-02 12:05:57 +0200

    ignore logging errors to db

--- src/lager_dal.erl
+++ src/lager_dal.erl
@@ -7,4 +7,4 @@
 
 create_check_table(Table, Columns, Opts)            -> imem_meta:create_check_table(Table, Columns, Opts).
 create_check_table(Table, ColumnSpecs, Opts, Owner) -> imem_meta:create_check_table(Table, ColumnSpecs, Opts, Owner).
-dirty_write(Table, Record)                          -> imem_meta:dirty_write(Table, Record).
+dirty_write(Table, Record)                          -> catch imem_meta:dirty_write(Table, Record).
